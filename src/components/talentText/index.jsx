88479f74fb472b2df911146cd51a2534f2d4193f
Utkarsh Kumar <ukyp2014@gmail.comm>
2025-01-29 18:57:58 +0530

    talent text fix

--- src/components/talentText/index.jsx
+++ src/components/talentText/index.jsx
@@ -1,12 +1,11 @@
 const TalentText = ({ text, left, top, rotation }) => {
     return (
       <p
-        className={`text-[24px] absolute left-[${left}px] rotate-[${rotation}deg] top-[${top}px] px-[48px] py-[8px] border-white bg-black/30 backdrop-blur-md text-white font-medium rounded-full shadow-md border border-white/20`}
+        style={{ left: `${left}px`, top: `${top}px`, transform: `rotate(${rotation}deg)` }}
+        className="text-[24px] absolute px-[48px] py-[8px] border-white bg-black/30 backdrop-blur-md text-white font-medium rounded-full shadow-md border border-white/20"
       >
         {text}
       </p>
     );
   };
-  
-export default TalentText;
   
